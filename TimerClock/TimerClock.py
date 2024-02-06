976675ef4f826f888725ead4499068979360b6cb
kusrohit <kusrohit@users.noreply.github.com>
2024-02-06 10:56:21 +0530

    files updated

--- TimerClock/TimerClock.py
+++ TimerClock/TimerClock.py
@@ -1,18 +1,21 @@
 # Timer clock
+# requires : playsound, wheel
 
 # This is works on only command line (not graphical)
 
 from playsound import playsound
 import time
 
-clear = "\033[2J"
-clearandreturn = "\033[H"
+CLEAR = "\033[2J"
+CLEAR_AND_RUN = "\033[H"
 
 # playsound("alarm.wav")
+
+
 def alarm(seconds):
     time_elapsed = 0
 
-    print(clear)
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -21,16 +24,13 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f'{clearandreturn}Alarm will sound in : {minutes_left:02d}:{seconds_left:02d}')
+        print(f'{CLEAR_AND_RUN}Alarm will sound in : {minutes_left:02d}:{seconds_left:02d}')
 
     playsound('alarm.wav')
+
 
 minutes = int(input('How many minutes to wait : '))
 seconds = int(input('How many seconds to wait : '))
 
 total_seconds = minutes * 60 + seconds
 alarm(total_seconds)
-
-
-
-# Credit : Tech with Tim (On Youtube)
